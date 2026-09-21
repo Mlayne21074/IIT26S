aff6cb879e3d9013a41269d547b2df6bd25c7fd5
Marco Layne <marco.layne@student.lab.fi>
2026-09-21 08:59:48 +0300

    Add A2 Python tasks

---
+++ Programming/Week 2 Tasks/A2-T2.py
@@ -0,0 +1,9 @@
+print("Program starting.")
+
+brand = input("Insert car brand: ")
+model = input("Insert car model: ")
+
+print("Car brand is", f'"{brand}"', "and the model is", sep=" ", end=" ")
+print(f"'{model}'.")
+
+print("Program ending.")
---
+++ Programming/Week 2 Tasks/A2_T1.py
@@ -0,0 +1,11 @@
+print("Program starting.")
+
+name = input("What is your name: ")
+number1 = float(input("Enter a floating point number: "))
+number2 = float(input("Enter second floating point number: "))
+
+product = number1 * number2
+
+print(f"{name} you gave numbers {number1} and {number2}")
+print(f"Multiplying first and second number will result in product {product:.2f}")
+print("Program ending.")
